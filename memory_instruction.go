da7af1853c0d87b98b3c3a2f705121a7f5f1d039
nxsang <nxsang@axonactive.com>
2023-08-24 19:17:23 +0700

    memory_instruction.go

--- memory_instruction.go
+++ memory_instruction.go
@@ -59,11 +59,19 @@ type memoryInstruction struct {
 	scope   string
 	iType   memoryInstructionType
 	segment memorySegment
-	offset  uint8
+	offset  int
 }
 
 func (m memoryInstruction) Compile() string {
+	if m.iType == push {
+		return compilePushInstruction(m)
+	}
 	return "hello"
+}
+
+func assignD(offset int) string {
+	return "@" + strconv.FormatInt(int64(offset), 0) + "\n" +
+		"D=A\n"
 }
 
 func pushToStack() string {
@@ -84,15 +92,83 @@ func popFromStack() string {
 		"M=M-1\n"
 }
 
-func assignD(offset int16) string {
-	return "@" + strconv.FormatInt(int64(offset), 0) + "\n" +
-		"D=A\n"
+func getValueFromArg(offset int) string {
+	return assignD(offset) +
+		"@ARG\n" +
+		"A=D+M\n" +
+		"D=M\n"
 }
 
-func getValueFromArg(offset uint8) string {
-	return assignD(int16(offset)) +
-		"@ARG\n" +
-		"A=M\n" +
-		"A=D+A\n" +
-		"D=A\n"
+func getValueFromLcl(offset int) string {
+	return assignD(int(offset)) +
+		"@LCL\n" +
+		"A=D+M\n" +
+		"D=M\n"
+}
+
+func getValueFromThis(offset int) string {
+	return assignD(offset) +
+		"@THIS\n" +
+		"A=D+M\n" +
+		"D=M\n"
+}
+
+func getValueFromThat(offset int) string {
+	return assignD(offset) +
+		"@THAT\n" +
+		"A=D+M\n" +
+		"D=M\n"
+}
+
+func getValueFromConst(offset int) string {
+	return assignD(offset)
+}
+
+func getValueFromStatic(offset int, scope string) string {
+	return "@" + scope + strconv.FormatInt(int64(offset), 0) + "\n" +
+		"D=M\n"
+}
+
+func getValueFromTemp(offset int) string {
+	return "@" + strconv.FormatInt(int64(7+offset), 0) + "\n" +
+		"D=M\n"
+}
+
+func getValueFromPointer(offset int) string {
+	switch offset {
+	case 0:
+		return "@THIS\n" +
+			"D=M\n"
+	case 1:
+		return "@THAT\n" +
+			"D=M\n"
+	default:
+		panic("invalid offset in pointer segment")
+	}
+}
+
+func compilePushInstruction(m memoryInstruction) string {
+	var result string
+	switch m.segment {
+	case arg:
+		result = getValueFromArg(m.offset)
+	case lcl:
+		result = getValueFromLcl(m.offset)
+	case this:
+		result = getValueFromThis(m.offset)
+	case that:
+		result = getValueFromThat(m.offset)
+	case static:
+		result = getValueFromStatic(m.offset, m.scope)
+	case constant:
+		result = getValueFromConst(m.offset)
+	case temp:
+		result = getValueFromTemp(m.offset)
+	case pointer:
+		result = getValueFromPointer(m.offset)
+	default:
+		panic("invalid segment")
+	}
+	result += pushToStack()
+	return result
 }
